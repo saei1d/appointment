66fc5586ecd13bde4414f03315a8a0720ad3e44c
saei1d <hehehe3bar@gmail.com>
2026-07-21 14:28:48 +0330

    update migs

--- notification/migrations/0001_initial.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.db import migrations, models
-import django.db.models.deletion
-
-class Migration(migrations.Migration):
-    initial = True
-    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL)]
-    operations = [migrations.CreateModel(name='Notification', fields=[('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),('title', models.CharField(max_length=120)),('message', models.TextField()),('notification_type', models.CharField(choices=[('system','System'),('appointment','Appointment'),('payment','Payment')], default='system', max_length=20)),('is_read', models.BooleanField(default=False)),('created_at', models.DateTimeField(auto_now_add=True)),('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='notifications', to=settings.AUTH_USER_MODEL))])]
--- review/migrations/0001_initial.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.db import migrations, models
-import django.core.validators
-import django.db.models.deletion
-
-class Migration(migrations.Migration):
-    initial = True
-    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL), ('provider','0001_initial')]
-    operations = [migrations.CreateModel(name='Review', fields=[('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),('rating', models.PositiveSmallIntegerField(validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(5)])),('comment', models.TextField(blank=True)),('provider_reply', models.TextField(blank=True)),('is_reported', models.BooleanField(default=False)),('is_removed', models.BooleanField(default=False)),('created_at', models.DateTimeField(auto_now_add=True)),('customer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='reviews', to=settings.AUTH_USER_MODEL)),('provider', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='reviews', to='provider.provider'))])]
